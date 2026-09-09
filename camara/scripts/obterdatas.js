f5ce1bccbae0eb51a50b79a9b70028209bf0314d
jaamfengland-dot <jaamfengland@gmail.com>
2026-09-09 17:24:53 -0300

    atualizado

--- camara/scripts/obterdatas.js
+++ camara/scripts/obterdatas.js
@@ -8,3 +8,21 @@ document.getElementById("anoatual").textContent = anoAtual;
 document.getElementById("ultimamodificacao").textContent =
     `ultima modificação: ${document.lastModified}`;
 
+const botãogrid = document.querySelector("#grid");
+const botãolista = document.querySelector("#lista");
+const exibir = document.querySelector("article");
+
+// O código a seguir poderia ser escrito de forma mais limpa. Como? Talvez tenhamos que simplificar nosso HTML e pensar em uma visualização padrão.
+
+botãogrid.addEventListener("click", () => {
+	// exemplo usando função arrow
+	exibir.classList.add("grid");
+	exibir.classList.remove("lista");
+});
+
+botãolista.addEventListener("click", mostrarlist); // exemplo usando função definida
+
+function mostrarlist() {
+	exibir.classList.add("lista");
+	exibir.classList.remove("grid");
+}
