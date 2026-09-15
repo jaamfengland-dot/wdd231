eca9561973551d000ee98cb668c7414321cc8f37
jaamfengland-dot <jaamfengland@gmail.com>
2026-09-15 13:37:40 -0300

    iu

--- Configuração e Estrutura/script/modulos.mjs
+++ Configuração e Estrutura/script/modulos.mjs
@@ -1,73 +1,22 @@
-const cursoBYUI = {
-  codigo: "WDD231",
-  nome: "Desenvolvimento Frontend para Web I",
-  secoes: [
-    {
-      numeroSecao: 1,
-      matriculados: 88,
-      instrutor: "Irmão Silva",
-    },
-    {
-      numeroSecao: 2,
-      matriculados: 81,
-      instrutor: "Irmã Pinheiro",
-    },
-    {
-      numeroSecao: 3,
-      matriculados: 95,
-      instrutor: "Irmã Oliveira",
-    },
-  ],
-  mudarMatriculas: function (numeroSecao, add = true) {
-    // Find the section with the given section number
-    const indiceSecao = this.secoes.findIndex(
-      (secao) => secao.numeroSecao == numeroSecao
-    );
-    if (indiceSecao >= 0) {
-      if (add) {
-        this.secoes[indiceSecao].matriculados++;
-      } else {
-        this.secoes[indiceSecao].matriculados--;
-      }
-      renderizarSecoes(this.secoes);
-    }
-  },
-};
+// Importações
+import cursoBYUI from './curso.mjs';
+import { definirSecaoSelecionada } from './secoes.mjs';
+import { definirTitulo, renderizarSecoes } from './saida.mjs';
 
-function definirSecaoSelecionada() {
-  const elementoSelect = document.querySelector("#numeroSecao");
-  cursoBYUI.secoes.forEach((secao) => {
-    const option = document.createElement("option");
-    option.value = secao.numeroSecao;
-    option.textContent = `${secao.numeroSecao}`;
-    elementoSelect.appendChild(option);
-  });
-}
-
-function definirTitulo(curso) {
-  document.querySelector("#nomeCurso").textContent = curso.nome;
-  document.querySelector("#codigoCurso").textContent = curso.codigo;
-}
-
-function renderizarSecoes(secoes) {
-  const html = secoes.map(
-    (secao) => `<tr>
-    <td>${secao.numeroSecao}</td>
-    <td>${secao.matriculados}</td>
-    <td>${secao.instrutor}</td></tr>`
-  );
-  document.querySelector("#secoes").innerHTML = html.join("");
-}
-
+// Receptores de Eventos (Botões)
 document.querySelector("#matricularEstudante").addEventListener("click", function () {
   const numeroSecao = Number(document.querySelector("#numeroSecao").value);
   cursoBYUI.mudarMatriculas(numeroSecao);
+  renderizarSecoes(cursoBYUI.secoes); // Atualiza a tela após matricular
 });
+
 document.querySelector("#removerEstudante").addEventListener("click", function () {
   const numeroSecao = Number(document.querySelector("#numeroSecao").value);
   cursoBYUI.mudarMatriculas(numeroSecao, false);
+  renderizarSecoes(cursoBYUI.secoes); // Atualiza a tela após remover
 });
 
+// Inicialização da página
 definirTitulo(cursoBYUI);
 definirSecaoSelecionada(cursoBYUI.secoes);
 renderizarSecoes(cursoBYUI.secoes);
